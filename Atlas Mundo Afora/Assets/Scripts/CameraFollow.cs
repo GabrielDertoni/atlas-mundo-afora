c6da783cfd3328ef31d6149e0ed1db9558c7d1b6
GabrielDertoni <gab.dertoni@gmail.com>
2022-10-03 22:14:56 -0300

    Add simple bike and ramp

---
+++ Atlas Mundo Afora/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    public Transform target;
+
+    [SerializeField]
+    public float cameraSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform selfTrasnform = GetComponent<Transform>();
+        Vector2 delta = target.position - selfTrasnform.position;
+        float speed = Mathf.Min(delta.magnitude, cameraSpeed);
+        delta.Normalize();
+        delta *= speed;
+        selfTrasnform.position += new Vector3(delta.x, delta.y, 0f);
+    }
+}
