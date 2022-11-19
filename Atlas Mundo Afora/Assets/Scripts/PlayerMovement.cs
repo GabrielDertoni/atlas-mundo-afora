98681111df00a8ca4cdd6829dd929d2abba30256
GabrielDertoni <gab.dertoni@gmail.com>
2022-11-19 17:59:31 -0300

    Add boost when the player does a flip

--- Atlas Mundo Afora/Assets/Scripts/PlayerMovement.cs
+++ Atlas Mundo Afora/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    enum State
+    {
+        Airborne = 0b00,
+        GroundedWithFrontWheel = 0b01,
+        GroundedWithBackWheel = 0b10,
+        GroundedWithBothWheels = 0b11,
+    }
+
     [SerializeField] private float m_JumpForce = 10f;
     [SerializeField] private float m_Torque = 2f;
     [SerializeField] private float m_MaxAngularVelocity = 200f;
-    [SerializeField] private float m_ComebackTorque = 1f;
+    // [SerializeField] private float m_ComebackTorque = 1f;
     [SerializeField] private float m_AngularDrag = 2f;
+    [SerializeField] private float m_BoostImpulse = 5f;
     [SerializeField] private Collider2D m_BackWheelColider;
     [SerializeField] private Collider2D m_FrontWheelColider;
+
+    private Quaternion m_PrevRotation;
+    private float m_CumulativeRotation = 0f;
+    private int m_CountFlipsWhileInAir = 0;
+    private State m_PrevState = State.Airborne;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody2D>().angularDrag = m_AngularDrag;
+        m_PrevRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -37,6 +52,41 @@ public class PlayerMovement : MonoBehaviour
             // If it is not grounded with both wheels, just stop rotating
             rb.angularVelocity = 0f;
         }
+
+        DetectFlips();
+    }
+
+    private State GetState()
+    {
+        int mask = 0;
+        if (IsGroundedWithFrontWheel()) mask |= 0b01;
+        if (IsGroundedWithBackWheel())  mask |= 0b10;
+        return (State)mask;
+    }
+
+    private void DetectFlips()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        float delta = Quaternion.Angle(m_PrevRotation, transform.rotation);
+        if (Mathf.Abs(m_CumulativeRotation) > 360f)
+        {
+            // We did a flip! Assum a single flip was made
+            m_CumulativeRotation %= 360f;
+            m_CountFlipsWhileInAir++;
+        }
+
+        State state = GetState();
+        // If the player is not airborne anymore, but it was in the previous frame and there was a flip.
+        if (state != 0 && m_PrevState == 0 && m_CountFlipsWhileInAir > 0)
+        {
+            rb.AddForce(Vector2.right * m_BoostImpulse, ForceMode2D.Impulse);
+            m_CountFlipsWhileInAir = 0;
+        }
+
+        m_CumulativeRotation += delta;
+        m_PrevRotation = transform.rotation;
+        m_PrevState = state;
     }
 
     private void Jump()
