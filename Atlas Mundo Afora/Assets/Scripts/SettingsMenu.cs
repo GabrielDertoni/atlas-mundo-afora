d201d636f6541f75dc8820c70a698c5cf949d9b9
GabrielDertoni <gab.dertoni@gmail.com>
2022-11-24 02:17:09 -0300

    Get audio options working

--- Atlas Mundo Afora/Assets/Scripts/SettingsMenu.cs
+++ Atlas Mundo Afora/Assets/Scripts/SettingsMenu.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    [SerializeField] private AudioMixer m_AudioMixer;
+    [SerializeField] private AudioMixer m_MasterAudioMixer;
+    [SerializeField] private AudioMixer m_MusicAudioMixer;
 
-    public void OnMasterVolumeChange(float volume)
+    [SerializeField] private Slider m_MasterAudioSlider;
+    [SerializeField] private Slider m_MusicAudioSlider;
+
+    public void OnMasterVolumeChange()
     {
-        m_AudioMixer.SetFloat("volume", volume);
+        m_MasterAudioMixer.SetFloat("volume", m_MasterAudioSlider.value);
+    }
+
+    public void OnMusicVolumeChange()
+    {
+        m_MusicAudioMixer.SetFloat("volume", m_MusicAudioSlider.value);
     }
 }
